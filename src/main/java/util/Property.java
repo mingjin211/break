f17fa9bd701599f3dcc69c923b1e42fdb9f4e149
yumj <yumj@chinatelecom.cn>
2018-10-13 10:43:19 +0800

    daily commit

---
+++ src/main/java/util/Property.java
@@ -0,0 +1,20 @@
+package util;
+
+// Property.java
+
+import java.util.Date;
+import java.util.Properties;
+
+public class Property {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        Properties p = System.getProperties();
+        p.list(System.out);
+        System.out.println("--- Memory Usage:");
+        Runtime rt = Runtime.getRuntime();
+        System.out.println("Total Memory = "
+                + rt.totalMemory()
+                + " Free Memory = "
+                + rt.freeMemory());
+    }
+}
