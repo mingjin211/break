1ab267f1221ca73c7381dbe3a2b2ee8c5a55a706
yumingjin <yumingjin@foxmail.com>
2019-05-15 18:15:15 +0800

    leetcode daily

---
+++ src/main/java/leet/medium/No17LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,31 @@
+package leet.medium;
+
+import java.util.List;
+
+/**
+ * No 53
+ * Code letter-combinations-of-a-phone-number
+ * Url https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ * Status pass
+ * Answer https://leetcode-cn.com/articles/letter-combinations-of-a-phone-number/
+ *
+ *  给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ *
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ *
+ * 示例:
+ *
+ * 输入："23"
+ * 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ */
+public class No17LetterCombinationsOfPhoneNumber {
+
+  public static List<String> letterCombinations(String digits) {
+    return null;
+  }
+
+  public static void main(String args[]){
+    System.out.println(letterCombinations("23"));
+  }
+
+}
